b57674979009bb6e9dea6707b7f8904cdb12c71e
WoodShack <woodhousie@gmail.com>
2025-07-21 11:25:36 -0400

    Add form events

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ let mouseEventElement = document.getElementById("mouse-event-btn");
 let mouseEventStatus = document.getElementById("mouse-event-status");
 let keyboardEventElement = document.getElementById("keyboard-event-input");
 let keyboardEventStatus = document.getElementById("keyboard-event-status");
+let formEventElement = document.getElementById("form-event-form");
+let formEventStatus = document.getElementById("form-event-status");
 
 //Functions
 function addMouseEvent(status){
@@ -11,6 +13,10 @@ function addMouseEvent(status){
 
 function addKeyboardEvent(status){
     keyboardEventStatus.innerHTML = keyboardEventStatus.innerHTML+"<br>Status: "+status;
+}
+
+function addFormEvent(status){
+    formEventStatus.innerHTML = "Status: "+status;
 }
 
 //Mouse event
@@ -37,4 +43,14 @@ keyboardEventElement.addEventListener("keyup", function(event) {
 
 keyboardEventElement.addEventListener("input", function(event) {
     addKeyboardEvent("Input changed");
+});
+
+//Form event
+formEventElement.addEventListener("submit", function(event) {
+    event.preventDefault();
+    addFormEvent("Submitted");
+});
+
+formEventElement.addEventListener("change", function(event) {
+    addFormEvent("Changed");
 });
