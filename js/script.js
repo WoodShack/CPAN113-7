0125f12f612f2df67a01451b2019968c51b79f90
WoodShack <woodhousie@gmail.com>
2025-07-21 11:14:50 -0400

    Add keyboard events

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 //Variables
 let mouseEventElement = document.getElementById("mouse-event-btn");
 let mouseEventStatus = document.getElementById("mouse-event-status");
+let keyboardEventElement = document.getElementById("keyboard-event-input");
+let keyboardEventStatus = document.getElementById("keyboard-event-status");
 
 //Functions
 function addMouseEvent(status){
     mouseEventStatus.innerHTML = "Status: "+status;
+}
+
+function addKeyboardEvent(status){
+    keyboardEventStatus.innerHTML = keyboardEventStatus.innerHTML+"<br>Status: "+status;
 }
 
 //Mouse event
@@ -18,4 +24,17 @@ mouseEventElement.addEventListener("mouseenter", function(event) {
 
 mouseEventElement.addEventListener("mouseleave", function(event) {
     addMouseEvent("Mouse leave");
+});
+
+//Keyboard event
+keyboardEventElement.addEventListener("keydown", function(event) {
+    addKeyboardEvent(event.key+ " Key down");
+});
+
+keyboardEventElement.addEventListener("keyup", function(event) {
+    addKeyboardEvent(event.key+" Key up");
+});
+
+keyboardEventElement.addEventListener("input", function(event) {
+    addKeyboardEvent("Input changed");
 });
